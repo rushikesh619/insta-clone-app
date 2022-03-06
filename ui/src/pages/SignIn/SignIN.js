d05c6786297d254277fee43cbf588c71c9d79bfa
Rushikesh Shinde <rishikeshshinde619@gmail.com>
2022-03-06 14:39:02 +0530

    changed status pic display and login field placeholder

--- ui/src/pages/SignIn/SignIN.js
+++ ui/src/pages/SignIn/SignIN.js
@@ -1,45 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../LoginPage/LoginPage.css";
-import axios from 'axios'
+import axios from "axios";
 
 class SignIN extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            username : null,
-            password: null
-         }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: null,
+      password: null,
+    };
+  }
 
-    login=()=>{
-        const data = {
-            username: this.state.username,
-            password: this.state.password
+  login = () => {
+    const data = {
+      username: this.state.username,
+      password: this.state.password,
+    };
+    axios.post("/api/users/login", data).then((response) => {
+      if (response.data) {
+        console.log(response.data.user);
+        if (response.data.user) {
+          for (const i in response.data) {
+            localStorage.setItem(i, JSON.stringify(response.data[i]));
+          }
+          window.location.replace("/home");
         }
-        axios.post('/api/users/login', data).then(response => {
-            if (response.data) {
-                console.log(response.data.user);
-                if (response.data.user) {
-                    for (const i in response.data) {
-                        localStorage.setItem(i, JSON.stringify(response.data[i]));
-                    }
-                    window.location.replace('/home');
-                }
-            }
-        }
-        )
-        window.replace()
-    }
+      }
+    });
+    window.replace();
+  };
 
-    render() { 
-        return ( 
-        <div>
-             <input className="logipage__text" onChange={(event)=>{this.setState({username: event.currentTarget.value})}} type="text" placeholder="Phone number, username, or email" />
-             <input className="logipage__text" onChange={(event)=>{this.setState({password: event.currentTarget.value})}}  type="password" placeholder="Password" />
-             <button className="login__button" onClick={this.login}>Log In</button>
-        </div> 
+  render() {
+    return (
+      <div>
+        <input
+          className="logipage__text"
+          onChange={(event) => {
+            this.setState({ username: event.currentTarget.value });
+          }}
+          type="text"
+          placeholder="username"
+        />
+        <input
+          className="logipage__text"
+          onChange={(event) => {
+            this.setState({ password: event.currentTarget.value });
+          }}
+          type="password"
+          placeholder="Password"
+        />
+        <button className="login__button" onClick={this.login}>
+          Log In
+        </button>
+      </div>
     );
-    }
+  }
 }
- 
+
 export default SignIN;
--- ui/src/pages/StatusBar/StatusBar.js
+++ ui/src/pages/StatusBar/StatusBar.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./StatusBar.css";
-import uploadimage from "../../images/statusadd.png";
+import pp4 from "../../images/pp4.jpeg";
 
 class StatusBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            statusList: []
-         }
-    }
-    
-    render() { 
-        return ( 
-        <div>
-            <div className="statusbar__container">
-            <div className="fileupload">
-               
-                    <img className="statusbar__upload" src={uploadimage} width="55px" height="55px" />
-            </div>
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      statusList: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="statusbar__container">
+          <div className="fileupload">
+            <img
+              className="statusbar__upload"
+              src={pp4}
+              width="55px"
+              height="55px"
+            />
+          </div>
         </div>
-         );
-    }
+      </div>
+    );
+  }
 }
- 
+
 export default StatusBar;
