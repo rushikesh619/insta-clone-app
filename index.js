204e963823ed60a32f3844ccd0c88d0c675703d9
Rushikesh Shinde <rishikeshshinde619@gmail.com>
2023-07-17 23:18:38 +0530

    changed ui root to s3 path

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ app.use(fileupload({
   useTempFiles: true
 }));
 
-app.get("*.*", express.static(__dirname + "/ui/build"));
+app.get("*.*", express.static("https://rushikesh-insta-clone-app.s3.ap-south-1.amazonaws.com/build"));
 
 api(app);
 
 app.all("*", function (req, res) {
-  res.status(200).sendFile(`/`, { root: __dirname + "/ui/build" });
+  res.status(200).sendFile(`/`, { root: "https://rushikesh-insta-clone-app.s3.ap-south-1.amazonaws.com/build" });
 });
 
 const port = process.env.PORT || 3001;
